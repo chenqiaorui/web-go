d2e82d154571231511eb29bfa9d868116591cb10
Your Name <you@example.com>
2023-03-03 10:48:19 +0800

    added mysql demo

--- main.go
+++ main.go
@@ -2,45 +2,33 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-        "html/template"
-	"strings"
-	"log"
+
+        "database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
-
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()  //解析参数，默认是不会解析的
-	fmt.Println(r.Form)  //这些信息是输出到服务器端的打印信息
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	fmt.Fprintf(w, "Hello astaxie!") //这个写入到w的是输出到客户端的
-}
-
-func login(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method) //get request method
-	if r.Method == "GET" {
-		t, _ := template.ParseFiles("login.gtpl")
-		t.Execute(w, nil)
-	} else {
-		r.ParseForm()
-		// logic part of log in
-		fmt.Println("username:", r.Form["username"])
-		fmt.Println("password:", r.Form["password"])
-	}
-}
 
 
 func main() {
-	http.HandleFunc("/", sayhelloName) //设置访问的路由
-	http.HandleFunc("/login", login)
 
-        err := http.ListenAndServe(":8090", nil) //设置监听的端口
+        db, err := sql.Open("mysql", "root:PXDN93VRKUm8TeE7@tcp(192.168.1.167:33069)/andongni?charset=utf8")
+        checkErr(err)
+
+	stmt, err := db.Prepare("INSERT INTO userinfo SET username=?,department=?,created=?")
+	checkErr(err)
+
+	res, err := stmt.Exec("astaxie", "研发部门", "2012-12-09")
+	checkErr(err)
+
+	id, err := res.LastInsertId()
+	checkErr(err)
+
+	fmt.Println(id)
+	
+	db.Close()
+}
+
+func checkErr(err error) {
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+		panic(err)
 	}
 }
